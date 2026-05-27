38aaebe18de2930d1918bbddd714003836d736ca
liwang0918-collab <liwang0918@gmail.com>
2026-05-27 22:23:33 +0800

    Change unlock status indicators in Netflix script

--- script/netflix.js
+++ script/netflix.js
@@ -1,14 +1,16 @@
 $httpClient.get(“https://www.netflix.com/title/81215567”, function(error, response, data) {
 
-let result = “未解锁”;
+let result = “❌”;
 
-if (!error && response.status == 200) {
-result = “已解锁”;
+if (!error && response.status === 200) {
+result = “✅”;
 }
 
 $done({
 title: “Netflix”,
-content: result
+content: result,
+icon: “tv”,
+“icon-color”: “#E50914”
 });
 
 });
